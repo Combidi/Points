54ac5dc20a667a0ab2a2038c3774b5dbfe7f8906
Peter Combee <petercombee@gmail.com>
2020-04-05 12:28:16 +0200

    Adding Xcode project

---
+++ Points/Points/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Points
+//
+//  Created by Peter Combee on 05/04/2020.
+//  Copyright © 2020 Combidi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
